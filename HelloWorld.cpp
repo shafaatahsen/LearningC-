3a4eadf3365284cf6df12cc7acfe2d05fbc0ac32
zanelias <shafaat.ahsen@gmail.com>
2017-11-04 20:07:31 -0500

    first HelloWorld Project

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"HEY, you, I'm alive!!!! Oh, and Hello World!\n";
+	cin.get();  // cin.get waits for user input to press enter
+}
