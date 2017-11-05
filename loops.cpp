182082aeea13fa14e36488e114cbb8423813dc70
zanelias <shafaat.ahsen@gmail.com>
2017-11-04 20:22:31 -0500

    just made a loop in C++

---
+++ loops.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std; //tells compiler we're using a group of functions that are part of the standard library
+
+int main()
+{
+	int age;
+
+	cout<<"Please input your age: ";
+	cin>> age;
+	cin.ignore();
+	if ( age < 100) {
+		cout<<"You are pretty young!\n";
+	}
+	else if (age == 100) {
+		cout<<"You are old\n";
+
+	}
+	else {
+		cout<<"You are really old\n";
+
+	}
+	cin.get();
+	
+}
